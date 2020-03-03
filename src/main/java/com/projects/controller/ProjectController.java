79c601122ad5edf15de051a5fb37359e47e5c424
Anass ELBAZ <anasselbaz0@gmail.com>
2020-03-03 17:12:33 +0100

    {All Projects} \ {Project Bean}

--- src/main/java/com/projects/controller/ProjectController.java
+++ src/main/java/com/projects/controller/ProjectController.java
@@ -1,10 +1,32 @@
 package com.projects.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.projects.entity.Project;
+import com.projects.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 public class ProjectController {
 
+    @Autowired
+    private ProjectService projectService;
 
+    @GetMapping("/projects")
+    public List<Project> getAll() {
+        return  projectService.getAll();
+    }
+
+    @PostMapping("/projects")
+    public Project addOrUpdate(@Valid @RequestBody Project p) {
+        return  projectService.addOrUpdate(p);
+    }
+
+    @DeleteMapping("/projects/delete/{id}")
+    public void delete(@PathVariable("id") Long id) {
+        projectService.delete(id);
+    }
 
 }
