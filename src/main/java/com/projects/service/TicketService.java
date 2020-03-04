d81afd45c2f1ad1aad5a110c9f6c0eec92764ce1
Anass ELBAZ <anasselbaz0@gmail.com>
2020-03-04 10:39:38 +0100

    [service] Ticket Service

--- src/main/java/com/projects/service/TicketService.java
+++ src/main/java/com/projects/service/TicketService.java
@@ -1,10 +1,28 @@
 package com.projects.service;
 
+import com.projects.entity.Ticket;
+import com.projects.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TicketService {
 
+    @Autowired
+    private TicketRepository ticketRepository;
 
+    public List<Ticket> getAll() {
+        return ticketRepository.findAll();
+    }
+
+    public Ticket addOrUpdate(Ticket u) {
+        return ticketRepository.save(u);
+    }
+
+    public void delete(Long id) {
+        ticketRepository.deleteById(id);
+    }
 
 }
