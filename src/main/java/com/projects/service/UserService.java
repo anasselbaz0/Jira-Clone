8910fb34d4c09f27d03cf472366b050fd0923c03
Anass ELBAZ <anasselbaz0@gmail.com>
2020-03-04 10:37:10 +0100

    [service] User Service

--- src/main/java/com/projects/service/UserService.java
+++ src/main/java/com/projects/service/UserService.java
@@ -1,10 +1,28 @@
 package com.projects.service;
 
+import com.projects.entity.User;
+import com.projects.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
 
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    public User addOrUpdate(User u) {
+        return userRepository.save(u);
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
 
 }
