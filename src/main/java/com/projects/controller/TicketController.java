b06cdce54f5213a015754dbc480657e348a08b76
Anass ELBAZ <anasselbaz0@gmail.com>
2020-03-04 10:56:26 +0100

    [controller] ticket & user controllers

--- src/main/java/com/projects/controller/TicketController.java
+++ src/main/java/com/projects/controller/TicketController.java
@@ -1,10 +1,32 @@
 package com.projects.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.projects.entity.Ticket;
+import com.projects.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 public class TicketController {
 
+    @Autowired
+    private TicketService ticketService;
 
+    @GetMapping("/tickets")
+    public List<Ticket> getAll() {
+        return  ticketService.getAll();
+    }
+
+    @PostMapping("/tickets")
+    public Ticket addOrUpdate(@Valid @RequestBody Ticket p) {
+        return  ticketService.addOrUpdate(p);
+    }
+
+    @DeleteMapping("/tickets/delete/{id}")
+    public void delete(@PathVariable("id") Long id) {
+        ticketService.delete(id);
+    }
 
 }
--- src/main/java/com/projects/controller/UserController.java
+++ src/main/java/com/projects/controller/UserController.java
@@ -1,10 +1,33 @@
 package com.projects.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.projects.entity.User;
+import com.projects.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public List<User> getAll() {
+        return  userService.getAll();
+    }
+
+    @PostMapping("/users")
+    public User addOrUpdate(@Valid @RequestBody User p) {
+        return  userService.addOrUpdate(p);
+    }
+
+    @DeleteMapping("/users/delete/{id}")
+    public void delete(@PathVariable("id") Long id) {
+        userService.delete(id);
+    }
 
 
 }
